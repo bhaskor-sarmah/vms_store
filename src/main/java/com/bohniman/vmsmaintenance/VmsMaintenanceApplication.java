c3c1b870fca43b6303dbac0980b3ac82980941cb
Bhaskor Jyoti Sarmah <bhaskor.sarmah@bohniman.com>
2020-07-02 19:08:25 +0530

    Initial Commit

---
+++ src/main/java/com/bohniman/vmsmaintenance/VmsMaintenanceApplication.java
@@ -0,0 +1,13 @@
+package com.bohniman.vmsmaintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmsMaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmsMaintenanceApplication.class, args);
+	}
+
+}
