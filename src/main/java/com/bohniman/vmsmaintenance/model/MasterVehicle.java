e8aaa3c8fcb576d7604e85d3563705f6b002005c
Bhaskor Jyoti Sarmah <bhaskor.sarmah@bohniman.com>
2020-07-14 21:45:23 +0530

    Bug Fixes

--- src/main/java/com/bohniman/vmsmaintenance/model/MasterVehicle.java
+++ src/main/java/com/bohniman/vmsmaintenance/model/MasterVehicle.java
@@ -1,7 +1,6 @@
 package com.bohniman.vmsmaintenance.model;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -14,11 +13,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-import org.hibernate.annotations.CreationTimestamp;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -31,7 +29,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class MasterVehicle extends Auditable{
+public class MasterVehicle extends Auditable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -63,10 +61,12 @@ public class MasterVehicle extends Auditable{
 
 	@Column(columnDefinition = "tinyint(1) default 1")
 	private Boolean status = true;
-	
-    @OneToMany(mappedBy = "masterVehicle", cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH }, fetch = FetchType.LAZY)
+
+	@OneToMany(mappedBy = "masterVehicle", cascade = { CascadeType.PERSIST, CascadeType.MERGE,
+			CascadeType.REFRESH }, fetch = FetchType.LAZY)
 	private List<TransVehicleHealth> health = new ArrayList<>();
-	
+
+	@JsonIgnore
 	@OneToMany(mappedBy = "masterVehicle", fetch = FetchType.LAZY)
-    private List<TransVehicleJobCard> jobCards = new ArrayList<>();
+	private List<TransVehicleJobCard> jobCards = new ArrayList<>();
 }
